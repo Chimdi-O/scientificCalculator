a67fce52b986630d16f8e52aed6a8f65d42eedb4
Chimdi-O <chimdioguejiofor58@example.com>
2024-08-19 12:50:22 +0100

    testing if it works or not

--- Definite_integrator.py
+++ Definite_integrator.py
@@ -1,5 +1,5 @@
 
-from indefinate_integration.py import * 
+from indefinate_integration import * 
 
 def definate_basic_integration(lim1,lim2,function): 
     integrated_function = basic_integration(function)[:-3 ] 
