2bb23ba1659fdfd25e6b3aeb8e4aa94d040a1cc8
Chimdi-O <136125797+Chimdi-O@users.noreply.github.com>
2024-08-19 12:39:20 +0100

    Update Definite_integrator.py
    Started on the definite integration file

--- Definite_integrator.py
+++ Definite_integrator.py
@@ -1,56 +1,7 @@
-def basic_integration(function): 
+
+from indefinate_integration.py import * 
+
+def definate_basic_integration(lim1,lim2,function): 
+    integrated_function = basic_integration(function)[:-3 ] 
     
-    function = function.split(" ")
 
-    endOfList = False 
-    listIndex = 0 
-    while endOfList == False: 
-
-        if function[listIndex] == "-": 
-            function[listIndex+1] =str(int(function[listIndex+1])*-1) 
-            function.pop(listIndex)
-            listIndex -= 1 
-        if function[listIndex] == "+": 
-            function.pop(listIndex)
-            listIndex -= 1 
-
-        listIndex += 1 
-
-        if listIndex ==  len(function): 
-            endOfList = True 
-
-    integratedFunction = []
-
-    for i in range(len(function)): 
-
-        if "x" not in function[i]: 
-            power = 0 
-            coefficient = int(function[i])
-        elif "**" not in function[i]: 
-            power = 1 
-            coefficient = int(function[i][:function[i].index("x")])
-        else: 
-            power = int(function[i][function[i].index("**")+1]) 
-            coefficient = int(function[i][:function[i].index("x")])
-
-            
-
-        
-
-        finalPower = power+1 
-        finalCoefficient = coefficient/finalPower
-
-        if str(finalCoefficient)[-2:] == ".0": 
-            finalCoefficient = int(finalCoefficient)
-
-        if finalPower == 0: 
-            integratedFunction.append(str(finalCoefficient)) 
-        
-        elif finalPower == 1: 
-            integratedFunction.append(str(finalCoefficient)+"x")
-
-        
-        else: 
-            integratedFunction.append(str(finalCoefficient)+"x**"+str(finalPower))
-
-    return integratedFunction + " + c" 
